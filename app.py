a9d5e78143fafa718d911ad2c48942805a631883
ConnorWhitmore-Lewis <ConnorWhitmore-Lewis@cats3.com>
2024-03-18 15:56:00 +0000

    asd

--- app.py
+++ app.py
@@ -30,33 +30,18 @@ def predict():
 
     df2 = pd.DataFrame()
 
-    columns_to_normalize = [ 'volume_obv', 'volume_cmf', 'volume_fi', 'volume_em',
-       'volume_sma_em', 'volume_vpt', 'volume_vwap', 'volume_mfi',
-       'volume_nvi', 'volatility_bbm', 'volatility_bbh', 'volatility_bbl',
-       'volatility_bbw', 'volatility_bbp', 'volatility_bbhi',
-       'volatility_bbli', 'volatility_kcc', 'volatility_kch', 'volatility_kcl',
-       'volatility_kcw', 'volatility_kcp', 'volatility_kchi',
-       'volatility_kcli', 'volatility_dcl', 'volatility_dch', 'volatility_dcm',
+    columns_to_normalize = [ 'volume_obv', 'volume_cmf', 
        'volatility_dcw', 'volatility_dcp', 'volatility_atr', 'volatility_ui',
-       'trend_macd', 'trend_macd_signal', 'trend_macd_diff', 'trend_sma_fast',
        'trend_sma_slow', 'trend_ema_fast', 'trend_ema_slow',
-       'trend_vortex_ind_pos', 'trend_vortex_ind_neg', 'trend_vortex_ind_diff',
-       'trend_trix', 'trend_mass_index', 'trend_dpo', 'trend_kst',
-       'trend_kst_sig', 'trend_kst_diff', 'trend_ichimoku_conv',
-       'trend_ichimoku_base', 'trend_ichimoku_a', 'trend_ichimoku_b',
-       'trend_stc', 'trend_adx', 'trend_adx_pos', 'trend_adx_neg', 'trend_cci',
-       'trend_visual_ichimoku_a', 'trend_visual_ichimoku_b', 'trend_aroon_up',
-       'trend_aroon_down', 'trend_aroon_ind', 'trend_psar_up',
        'trend_psar_down', 'trend_psar_up_indicator',
        'trend_psar_down_indicator', 'momentum_rsi', 'momentum_stoch_rsi',
-       'momentum_stoch_rsi_k', 'momentum_stoch_rsi_d', 'momentum_tsi',
-       'momentum_uo', 'momentum_stoch', 'momentum_stoch_signal', 'momentum_wr',
-       'momentum_ao', 'momentum_roc', 'momentum_ppo', 'momentum_ppo_signal',
-       'momentum_ppo_hist', 'momentum_pvo', 'momentum_pvo_signal',
-       'momentum_pvo_hist', 'momentum_kama', 'others_dr', 'others_dlr',
+       'momentum_stoch_rsi_k', 'momentum_stoch_rsi_d', 
+       'momentum_uo', 
+       'momentum_ao', 
+       'momentum_ppo_hist', 
+       'momentum_pvo_hist', 
        'others_cr'] 
-    
-    lookbacks = 10;
+    lookbacks = 5;
 
     # df2.insert(loc = 0, column="Low", value=df["Low"])
     # df2.insert(loc = 1, column="High", value=df["High"])
@@ -99,7 +84,7 @@ def predict():
     # Reshape the input data
     data2 = np.array([df3[col] for col in columns_to_normalize])
     
-    input_data = data2.T.reshape(1, lookbacks, 85)
+    input_data = data2.T.reshape(1, lookbacks, len(columns_to_normalize))
     # Make predictions on the new data
     predictions = model.predict(input_data, batch_size = 1)
     shouldBuy = predictions
