741bc5734a41b27400872672d5619d88e14d336b
ConnorWhitmore-Lewis <ConnorWhitmore-Lewis@cats3.com>
2024-03-18 19:10:45 +0000

    321

--- app.py
+++ app.py
@@ -99,7 +99,7 @@ def predict():
     # Reshape the input data
     data2 = np.array([df3[col] for col in columns_to_normalize])
     
-    input_data = data2.T.reshape(1, lookbacks, 85)
+    input_data = data2.reshape(1, lookbacks, 85)
     print(input_data[-1])
     # Make predictions on the new data
     predictions = model.predict(input_data, batch_size = 1)
